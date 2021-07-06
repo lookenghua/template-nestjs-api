f5087f815fa43a09f965d2381aad507fdccdf64f
lookenghua <lookenghua@gmail.com>
2021-07-06 08:19:37 +0800

    feat:新增数据库配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,35 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { UserModule } from '_module/user/user.module'
 import { AuthModule } from '_module/auth/auth.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import configuration from './config/configuration'
 
 @Module({
-  imports: [UserModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('database.mysql.host'),
+        port: +config.get<number>('database.mysql.port'),
+        username: config.get('database.mysql.username'),
+        password: config.get('database.mysql.password'),
+        database: config.get('database.mysql.database'),
+        entities: [__dirname + '/model/*.model{.ts,.js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: true,
+        keepConnectionAlive: true,
+      }),
+    }),
+    UserModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
