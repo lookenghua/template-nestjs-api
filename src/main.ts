73edc04ed0dab289974cb15da72a0d55366c0239
lookenghua <lookenghua@gmail.com>
2023-06-16 12:18:22 +0800

    chore: 升级到10.x版本

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import { ValidationErrorException } from '_common/exception/validation-error.exception'
 
-declare const module: any
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
@@ -16,10 +14,6 @@ async function bootstrap() {
     }),
   )
   await app.listen(process.env.PORT || 3000)
-  if (module.hot) {
-    module.hot.accept()
-    module.hot.dispose(() => app.close())
-  }
 }
 
 bootstrap().then(() => {
